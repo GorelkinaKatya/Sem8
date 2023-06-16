83041c8d31feef79255ace982e9d675495fb0fe3
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-06-16 14:13:10 +0300

    done Task 54

---
+++ Task54/Program.cs
@@ -0,0 +1,47 @@
+﻿void InputMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+void PrintMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+void MatrixExchange(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    (matrix[i, j], matrix[i, j + 1]) = (matrix[i, j + 1], matrix[i, j]);
+                    MatrixExchange(matrix);
+                }        
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0],size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальная матрица");
+PrintMatrix(matrix);
+MatrixExchange(matrix);
+Console.WriteLine("Конечная матрица");
+PrintMatrix(matrix);
