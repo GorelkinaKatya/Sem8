0c589a7ff5a479f0e1230fde80adbc44c2d1cb7b
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-06-16 15:20:18 +0300

    done Task 60

---
+++ Task60/Program.cs
@@ -0,0 +1,31 @@
+﻿void InputArray(int [,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+                array[i, j, k] = new Random().Next(10, 100);
+        }
+    }
+}
+
+void PrintArray(int [,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+                Console.Write($"{array[j, k, i]} ({j},{k},{i})\t");
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,,] array = new int[size[0],size[1],size[2]];
+InputArray(array);
+PrintArray(array);
